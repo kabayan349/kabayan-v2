6ef105e7bbe1021772fe79e2a7689948673a10fc
kabayan349 <67649284+kabayan349@users.noreply.github.com>
2020-07-15 11:52:09 +0700

    locat

--- function4.php
+++ function4.php
@@ -10,7 +10,7 @@ function request($url, $token = null, $data = null, $pin = null, $otpsetpin = nu
     $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
     $header[] = "Connection: keep-alive";
     $header[] = "X-User-Locale: id_ID";
-    $header[] = "X-Location: -7.32789,112.73776";
+    $header[] = "X-Location: -6.189454, 106.841180";
     $header[] = "X-Location-Accuracy: 3.0";
     if ($pin):
     $header[] = "pin: $pin";
